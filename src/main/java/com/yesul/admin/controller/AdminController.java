c99556f91e9a38c639cbc1df8aa64f7961a6e99e
younggalee <rkduddl1717@gmail.com>
2025-06-26 19:28:26 +0900

    fix: 뒤로가기시 sidebar 로드되지 않는 문제 수정
    뒤로가기를 누르면 동적 JS가 재실행되지 않아 사이드바가 보이지 않는 문제가 있었음. 뒤로가기 발생시, window.onload를 발생시켜 해결
    
    fixes #3

--- src/main/java/com/yesul/admin/controller/AdminController.java
+++ src/main/java/com/yesul/admin/controller/AdminController.java
@@ -16,6 +16,27 @@ public class AdminController {
     public void loginPage(){}
 
     @GetMapping("/dashboard")
-    public void dashboardPage(){}
+    public String dashboardPage(){ return "admin/dashboard"; }
+
+    // --------------- 레이아웃 -----------------
+    @GetMapping("/board")
+    public String boardPage(){ return "admin/board/list";}
+
+    @GetMapping("/alcohol")
+    public String alcoholPage(){ return "admin/alcohol/list";}
+
+    @GetMapping("/sale")
+    public String salePage(){ return "admin/sale/list";}
+
+    @GetMapping("/userInfo")
+    public String userInfoPage(){ return "admin/userInfo/list";}
+
+    @GetMapping("/review")
+    public String reviewPage(){ return "admin/review/list";}
+
+    @GetMapping("/chat")
+    public String chatPage(){ return "admin/chat/list";}
+
+    // --------------- 관리 기능 ----------------
 
 }
