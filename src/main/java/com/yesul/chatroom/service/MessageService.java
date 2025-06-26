dbcb62f98b51dcc7c47420fa26b445a4129dff59
이승주 <eun07213@gmail.com>
2025-06-26 18:13:49 +0900

    feat: 메시지 서비스 추가

--- src/main/java/com/yesul/chatroom/service/MessageService.java
+++ src/main/java/com/yesul/chatroom/service/MessageService.java
@@ -3,7 +3,6 @@ package com.yesul.chatroom.service;
 import com.yesul.chatroom.model.entity.Message;
 import com.yesul.chatroom.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -17,7 +16,8 @@ public class MessageService {
     private final MessageRepository messageRepository;
 
     public Slice<Message> getMessagesWithCursor(Long chatRoomId, Long lastMessageId, int size) {
-        return messageRepository.getMessagesWithCursor(chatRoomId, lastMessageId, size);
+        Pageable pageable = PageRequest.of(0, size, Sort.by(Sort.Direction.DESC, "id"));
+        return messageRepository.getMessagesWithCursor(chatRoomId, lastMessageId, pageable);
     }
 
 }
