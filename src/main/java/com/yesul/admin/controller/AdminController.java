b34067078956a7bb07687546a4d386bb099841f5
younggalee <rkduddl1717@gmail.com>
2025-06-30 12:06:48 +0900

    feat: 회원상태변경 기능 Controller 추가
    목록의 회원정보중 회원의 '상태'를 정지시키거나 정지 해제시킬 수 있는 관리자기능입니다.
    
    Refs: #17

--- src/main/java/com/yesul/admin/controller/AdminController.java
+++ src/main/java/com/yesul/admin/controller/AdminController.java
@@ -6,11 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -68,5 +68,19 @@ public class AdminController {
     public String chatPage(){ return "admin/chatroom/list";}
 
     // --------------- 관리 기능 ----------------
+
+    @PostMapping("/userInfo/{userId}/status")
+    @ResponseBody
+    public ResponseEntity<String> updateUserStatus(
+            @PathVariable Long userId,
+            @RequestParam String displayStatus,
+            RedirectAttributes redirectAttributes) {
+
+        adminService.updateUserStatus(userId, displayStatus);
+
+        return ResponseEntity.ok("회원 상태가 변경되었습니다.");
+    }
+
+
 
 }
