4d6f901d03aa6ad667dae8d1a937c0cd9aebe950
이승주 <eun07213@gmail.com>
2025-06-30 10:18:46 +0900

    feat: ChatRoomRepository 수정

--- src/main/java/com/yesul/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/yesul/chatroom/repository/ChatRoomRepository.java
@@ -4,7 +4,9 @@ import com.yesul.chatroom.model.entity.ChatRoom;
 import com.yesul.user.model.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByUser(User user);
 }
