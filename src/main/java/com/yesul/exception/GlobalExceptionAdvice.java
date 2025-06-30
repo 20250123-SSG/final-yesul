a54736eeb786f5af5e04d7776a2dee0ab67bcc88
younggalee <rkduddl1717@gmail.com>
2025-06-30 12:11:27 +0900

    feat: 회원상태변경 Service 구현 및 예외발생
    Enum을 통해 회원상태를 매핑하여 사용하고 비동기방식으로 회원정보수정을 구현함.
    
    Refs: #17

--- src/main/java/com/yesul/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/yesul/exception/GlobalExceptionAdvice.java
@@ -1,7 +1,10 @@
 package com.yesul.exception;
 
+import com.yesul.exception.handler.UpdateFailedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @ControllerAdvice
@@ -13,6 +16,13 @@ public class GlobalExceptionAdvice {
 //        log.warn("[EntityNotFoundException] {} {} - {}", request.getMethod(), request.getRequestURI(), e.getMessage());
 //        return "404.html";
 //    }
+
+    @ExceptionHandler(UpdateFailedException.class)
+    public String handleUserStatusUpdateFailed(UpdateFailedException e,
+                                               RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error", e.getMessage());
+        return "redirect:/admin/userInfo/list";
+    }
 
 
 
