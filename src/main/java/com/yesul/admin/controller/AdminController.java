f99c9eaf732f72df83e4f0d24edddb7597d7b6d1
younggalee <rkduddl1717@gmail.com>
2025-07-02 00:10:27 +0900

    refactor: 관리자 회원관리 영역 기능 단위 리팩토링
    Refs: #44

--- src/main/java/com/yesul/admin/controller/AdminController.java
+++ src/main/java/com/yesul/admin/controller/AdminController.java
@@ -1,26 +1,17 @@
 package com.yesul.admin.controller;
 
-import com.yesul.admin.model.dto.userInfo.UserInfoListAdminDto;
-import com.yesul.admin.service.AdminService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 @Controller
-public class AdminController {
-
-    private final AdminService adminService;
+public class AdminController {  // 관리자 로그인, Dashboard, 공지사항
 
     @GetMapping("/login")
     public void loginPage(){}
@@ -41,46 +32,4 @@ public class AdminController {
         }
         return "admin/dashboard";
     }
-
-    // --------------- 레이아웃 -----------------
-    @GetMapping("/board")
-    public String boardPage(){ return "admin/board/list";}
-
-    @GetMapping("/alcohol")
-    public String alcoholPage(){ return "admin/alcohol/list";}
-
-    @GetMapping("/sale")
-    public String salePage(){ return "admin/sale/list";}
-
-    @GetMapping("/userInfo")
-    public String userInfoPage(Model model){
-
-        List<UserInfoListAdminDto> userInfoList = adminService.getAllUsersInfo();
-        model.addAttribute("userInfoList", userInfoList);
-
-        return "admin/userInfo/list";
-    }
-
-    @GetMapping("/review")
-    public String reviewPage(){ return "admin/review/list";}
-
-    @GetMapping("/chatroom")
-    public String chatPage(){ return "admin/chatroom/list";}
-
-    // --------------- 관리 기능 ----------------
-
-    @PostMapping("/userInfo/{userId}/status")
-    @ResponseBody
-    public ResponseEntity<String> updateUserStatus(
-            @PathVariable Long userId,
-            @RequestParam String displayStatus,
-            RedirectAttributes redirectAttributes) {
-
-        adminService.updateUserStatus(userId, displayStatus);
-
-        return ResponseEntity.ok("회원 상태가 변경되었습니다.");
-    }
-
-
-
 }
