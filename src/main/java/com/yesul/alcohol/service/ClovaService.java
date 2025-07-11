3947300e1a8ec1f87900b505c88fa4810136448e
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-07-11 16:05:27 +0900

    update: 버전 업데이트

--- src/main/java/com/yesul/alcohol/service/ClovaService.java
+++ src/main/java/com/yesul/alcohol/service/ClovaService.java
@@ -18,12 +18,12 @@ public class ClovaService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public String callClovaAPI(ClovaAskRequestDto askDto) {
-        String url = "https://clovastudio.stream.ntruss.com/testapp/v1/skillsets/fv46jc9v/versions/17/final-answer";
+        String url = "https://clovastudio.stream.ntruss.com/testapp/v1/skillsets/fv46jc9v/versions/18/final-answer";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer nv-ae4562f3d5a54905a8ae4563a0ad9ce7eYf5");
-        headers.set("X-NCP-CLOVASTUDIO-REQUEST-ID", "4cbca115b1b94c61bc0120bfc73458dc");
+        headers.set("X-NCP-CLOVASTUDIO-REQUEST-ID", "d45d3610c48e459bb10f4bcba5488733");
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 
         ClovaRequestDto dto = ClovaRequestDto.builder()
