03fd29808a95b758cabce21405ad7a3872e071d0
ibe6 <nxnxnxnx626@gmail.com>
2025-07-16 10:27:43 +0900

    fix: 유저 엔티티 변경

--- src/main/java/com/yesul/user/model/entity/User.java
+++ src/main/java/com/yesul/user/model/entity/User.java
@@ -64,8 +64,8 @@ public class User extends BaseTimeEntity {
     @Column(name = "description")
     private String description;
 
-    @Column(name = "points", nullable = false)
-    private Integer points = 0;
+    @Column(name = "point", nullable = false)
+    private Integer point = 0;
 
     public void encodePassword(PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(this.password);
@@ -105,17 +105,17 @@ public class User extends BaseTimeEntity {
 
     // 포인트 적립
     public void earnPoint(Integer amount) {
-        this.points += amount;
+        this.point += amount;
     }
 
     // 포인트 차감
     public void usePoint(Integer amount) {
-        if (this.points < amount) throw new IllegalArgumentException("포인트 부족");
-        this.points -= amount;
+        if (this.point < amount) throw new IllegalArgumentException("포인트 부족");
+        this.point -= amount;
     }
 
     @Builder
-    public User(Long id, String email, String password, String name, String nickname, String birthday, String address, Character type, Character status, String profile, String provider, String providerId, String description, Integer points) {
+    public User(Long id, String email, String password, String name, String nickname, String birthday, String address, Character type, Character status, String profile, String provider, String providerId, String description, Integer point) {
         this.id = id;
         this.password = password;
         this.name = name;
@@ -129,6 +129,6 @@ public class User extends BaseTimeEntity {
         this.provider = provider;
         this.providerId = providerId;
         this.description = description;
-        this.points = points;
+        this.point = point;
     }
 }
